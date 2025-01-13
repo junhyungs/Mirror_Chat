c61df0eec4fcceb29f2dc617f41e42ea182badc8
junhyungs <jin05003@naver.com>
2025-01-14 02:32:29 +0900

    [feat] _NetworkAuthenticator
    NetworkAuthenticator 상속으로 변경

--- Assets/Script/_NetworkAuthenticator.cs
+++ Assets/Script/_NetworkAuthenticator.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class _NetworkAuthenticator : MonoBehaviour
+public class _NetworkAuthenticator : NetworkAuthenticator
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
