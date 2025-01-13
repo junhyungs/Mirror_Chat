e593c85f60dc97e4267304610e3e2baae5e03a0d
junhyungs <jin05003@naver.com>
2025-01-13 23:37:02 +0900

    [feat] Login_popUp
    유저 이름 입력처리. null, 빈 문자열, 공백인지 확인 후 버튼 활성화.

--- Assets/Script/Login_PopUp.cs
+++ Assets/Script/Login_PopUp.cs
@@ -7,7 +7,7 @@ using Mirror;
 public class Login_PopUp : MonoBehaviour
 {
     [Header("NetworkAddress")]
-    [SerializeField] internal InputField _networkAddress;
+    [SerializeField] internal InputField _networkAddress; //internal - 같은 어셈블리(ex - nameSpace)내에서 public과 같은 동작. 단, 다른 어셈블리에서는 접근할 수 없다.
     [SerializeField] internal InputField _userName;
 
     [SerializeField] internal Button _startHostButton;
@@ -24,18 +24,54 @@ public class Login_PopUp : MonoBehaviour
         Instance = this;
     }
 
+    private void OnEnable()
+    {
+        _userName.onValueChanged.AddListener(OnValueChanged_ToggleButton);
+    }
+
+    private void OnDisable()
+    {
+        _userName.onValueChanged.RemoveListener(OnValueChanged_ToggleButton);
+    }
+
     private void Start()
     {
         SetDefaultNetworkAddress();
     }
 
-    private void SetDefaultNetworkAddress()
+    private void Update()
     {
-        if (string.IsNullOrWhiteSpace(NetworkManager.singleton.networkAddress))
+        CheckNetworkAddressOnUpdate();
+    }
+
+    private void SetDefaultNetworkAddress() //NetworkManager.singleton.networkAddress설정.
+    {
+        if (string.IsNullOrWhiteSpace(NetworkManager.singleton.networkAddress)) //현재 네트워크 주소가 null, 빈 문자열, 공백인지 확인.
         {
-            NetworkManager.singleton.networkAddress = "localHost";
+            NetworkManager.singleton.networkAddress = "localHost"; //조건에 해당되면 주소를 "localHost"로 설정.
         }
 
-        _originNetworkAddress = NetworkManager.singleton.networkAddress;
+        _originNetworkAddress = NetworkManager.singleton.networkAddress; //원본 네트워크 주소 저장.
+    }
+
+    private void CheckNetworkAddressOnUpdate() //Network 주소 변화 감지.
+    {
+        if (string.IsNullOrWhiteSpace(NetworkManager.singleton.networkAddress)) //주소가 null, 빈 문자열, 공백이 되면 저장해둔 원본 주소로 설정.
+        {
+            NetworkManager.singleton.networkAddress = _originNetworkAddress;
+        }
+
+        if(_networkAddress.text != NetworkManager.singleton.networkAddress) //주소 입력 Text가 현재 주소와 같지 않다면 현재 주소로 설정.
+        {
+            _networkAddress.text = NetworkManager.singleton.networkAddress;
+        }
+    }
+
+    private void OnValueChanged_ToggleButton(string userName)
+    {
+        bool userNameValue = string.IsNullOrWhiteSpace(userName);
+
+        _startHostButton.interactable = userNameValue;
+        _startClientButton.interactable = userNameValue;
     }
 }
