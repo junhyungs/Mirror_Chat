1b47a0278e91c769e3dbc15066ccd5d9cb5c09c1
junhyungs <jin05003@naver.com>
2025-01-14 00:12:11 +0900

    [feat] Chatting_UI
    NetworkBehaviour 상속으로 변경

--- Assets/Script/Chatting_UI.cs
+++ Assets/Script/Chatting_UI.cs
@@ -2,12 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
 
-public class Chatting_UI : MonoBehaviour
+public class Chatting_UI : NetworkBehaviour
 {
     [Header("ChattingUI")]
     [SerializeField] private Text _chatHistroy;
     [SerializeField] private Scrollbar _chatScrollbar;
     [SerializeField] private InputField _chatMessage;
     [SerializeField] private Button _sendButton;
+
+    public override void OnStartServer()
+    {
+        
+    }
+
+    public override void OnStartClient()
+    {
+        
+    }
 }
