91b8320dde9dfc5bf08cc9dffaeec10a4a303565
junhyungs <jin05003@naver.com>
2025-01-14 01:43:58 +0900

    [feat] _NetworkManager
    NetworkManager 상속으로 변경.
    그외 OnValueChanged_SetHostName함수 작성.

--- Assets/Script/_NetworkManager.cs
+++ Assets/Script/_NetworkManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class _NetworkManager : MonoBehaviour
+public class _NetworkManager : NetworkManager
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnValueChanged_SetHostName(string hostName)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnServerDisconnect(NetworkConnectionToClient clientNetworkInformation)
     {
-        
+        base.OnServerDisconnect(clientNetworkInformation);
+    }
+
+    public override void OnClientDisconnect()
+    {
+        base.OnClientDisconnect();
     }
 }
