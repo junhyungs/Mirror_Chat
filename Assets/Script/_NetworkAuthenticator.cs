0fbe5184cd412a09ce99d7ed92054370c590847a
junhyungs <jin05003@naver.com>
2025-01-14 09:53:21 +0900

    [feat] _NetworkAuthenticator
    네트워크 인증자 클라이언트 인증 요청 처리 핸들러 등록.

--- Assets/Script/_NetworkAuthenticator.cs
+++ Assets/Script/_NetworkAuthenticator.cs
@@ -5,15 +5,15 @@ using Mirror;
 
 public class _NetworkAuthenticator : NetworkAuthenticator
 {
-    public struct AuthRequestMessage : NetworkMessage
+    public struct AuthRequestMessage : NetworkMessage //클라이언트가 서버에 인증을 요청할 때 사용되는 구조체. 
     {
-        public string _authUserName;
+        public string _authUserName; //클라이언트가 서버에 전달하는 유저 이름.
     }
 
-    public struct AuthResiveMessage : NetworkMessage
+    public struct AuthResiveMessage : NetworkMessage //서버가 클라이언트에게 인증 결과를 응답할 때 사용되는 구조체.
     {
-        public byte _code;
-        public string _message;
+        public byte _code; //인증 결과를 나타내는 변수. (Ex 성공 = 0, 실패 = 1)
+        public string _message; //인증 결과 메시지.
     }
 
     #region Server
@@ -26,7 +26,8 @@ public class _NetworkAuthenticator : NetworkAuthenticator
 
     public override void OnStartServer()
     {
-        
+        //서버가 클라이언트로부터 AuthRequestMessage를 수신할 경우, OnAuthRequestMessage 메서드가 실행되도록 하는 핸들러 등록. false -> 권한설정. 인증이 없어도 메시지 처리가 가능함을 의미.
+        NetworkServer.RegisterHandler<AuthRequestMessage>(OnAuthRequestMessage, false); 
     }
 
     public override void OnStopServer()
@@ -39,7 +40,7 @@ public class _NetworkAuthenticator : NetworkAuthenticator
         
     }
 
-    public void OnAuthRequestMessage(NetworkConnectionToClient clientNetworkInformation)
+    public void OnAuthRequestMessage(NetworkConnectionToClient clientNetworkInformation, AuthRequestMessage message)
     {
 
     }
